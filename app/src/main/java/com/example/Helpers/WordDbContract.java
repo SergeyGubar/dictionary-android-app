b0161ebfd58fbca9dadcefa0b978a3699213cb0b
Gubar Sergey <serhii.hubar@nure.ua>
2017-08-18 17:42:50 +0300

    Start working with DB

---
+++ app/src/main/java/com/example/Helpers/WordDbContract.java
@@ -0,0 +1,21 @@
+package com.example.Helpers;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Sergey on 8/18/2017.
+ */
+
+public class WordDbContract {
+    private WordDbContract() {
+
+    }
+
+    public static final class WordEntry implements BaseColumns {
+        public static final String TABLE_NAME = "words";
+        public static final String COLUMN_CATEGORY = "wordCategory";
+        public static final String COLUMN_ENG_WORD = "engWord";
+        public static final String COLUMN_RUS_WORD = "timestamp";
+    }
+
+}
